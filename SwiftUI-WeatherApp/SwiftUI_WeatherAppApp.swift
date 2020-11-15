f620907b84b92a63a1dcfb6c269d81827352eb5e
BlayckPanthers <fabienlebon974@gmail.com>
2020-11-15 13:31:56 +0100

    Initial Commit

---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Fabien Lebon on 15/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
